76487b3deebd3b3c54626a76e0127e138e05bafc
Javan Pohl <72358420+javan-pohl@users.noreply.github.com>
2021-03-11 16:04:35 -0700

    LightModule Built Constructed the css for the light module

--- src/components/LightModule.jsx
+++ src/components/LightModule.jsx
@@ -3,9 +3,15 @@ import React, { useState } from "react";
 export default function LightModule() {
   return (
     <div className="light-module">
-      <div className="light red" />
-      <div className="light yellow" />
-      <div className="light green" />
+      <div className="light-module-square">
+        <div className="light red"></div>
+      </div>
+      <div className="light-module-square">
+        <div className="light yellow"></div>
+      </div>
+      <div className="light-module-square">
+        <div className="light green"></div>
+      </div>
     </div>
   );
 }
