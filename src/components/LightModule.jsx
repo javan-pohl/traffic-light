298f7287775005b2c3b3960917b0b632b24bd4ba
Javan Pohl <72358420+javan-pohl@users.noreply.github.com>
2021-03-11 17:11:41 -0700

    light functions Added functions to the light module to change the class name for the lights depending on the input color prop

--- src/components/LightModule.jsx
+++ src/components/LightModule.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 
-export default function LightModule() {
+export default function LightModule(color) {
+  const isRed = () => (color === "red" ? "light red" : "light white");
+  const isYellow = () => (color === "yellow" ? "light yellow" : "light white");
+  const isGreen = () => (color === "green" ? "light green" : "light white");
+
   return (
     <div className="light-module">
       <div className="light-module-square">
-        <div className="light red"></div>
+        <div className={isRed()}></div>
       </div>
       <div className="light-module-square">
-        <div className="light yellow"></div>
+        <div className={isYellow()}></div>
       </div>
       <div className="light-module-square">
-        <div className="light green"></div>
+        <div className={isGreen()}></div>
       </div>
     </div>
   );
